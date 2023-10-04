c6e236fb57b6a2011a38c9922b73e8120806bada
ameys8 <ameysawant2806@gmail.com>
2023-10-04 21:14:20 +0530

    added event listener

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 console.log("Welcome to Spotify!");
 let songIndex = 0;
-let audioElement = new Audio('songs/faded.mp3');
-let masterplay = document.getElementById('masterplay');
-let myProgressbar = document.getElementById('myProgressbar');
-
+let audioElement = new Audio("songs/faded.mp3");
+let masterplay = document.getElementById("masterplay");
+let myProgressbar = document.getElementById("myProgressbar");
 
 let songs = [
-  {songName: "Faded", filepath: "songs/faded.mp3"},
-  {songName: "Faded", filepath: "songs/faded.mp3"},
-  {songName: "Faded", filepath: "songs/faded.mp3"},
-  {songName: "Faded", filepath: "songs/faded.mp3"},
-  {songName: "Faded", filepath: "songs/faded.mp3"},
-  {songName: "Faded", filepath: "songs/faded.mp3"},
-  
-]
+  { songName: "Faded", filepath: "songs/faded.mp3" },
+  { songName: "Faded", filepath: "songs/faded.mp3" },
+  { songName: "Faded", filepath: "songs/faded.mp3" },
+  { songName: "Faded", filepath: "songs/faded.mp3" },
+  { songName: "Faded", filepath: "songs/faded.mp3" },
+  { songName: "Faded", filepath: "songs/faded.mp3" },
+];
 //Play/pause
-masterplay.addEventListener('click', ()=>{
-  if (audioElement.paused || audioElement.currentTime<=0) {
+masterplay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
     audioElement.play();
-    masterplay.classList.remove('fa-play-circle');
-    masterplay.classList.add('fa-pause-circle');
-  }
-  else{
+    masterplay.classList.remove("fa-play-circle");
+    masterplay.classList.add("fa-pause-circle");
+  } else {
     audioElement.pause();
-    masterplay.classList.remove('fa-pause-circle');
-    masterplay.classList.add('fa-play-circle');
+    masterplay.classList.remove("fa-pause-circle");
+    masterplay.classList.add("fa-play-circle");
   }
-})
+});
 // Listen to events
-myProgressbar.addEventListener('timeupdate', ()=> {
-  console.log('timeupdate');
+audioElement.addEventListener('timeupdate', () => {
+  progress = parseInt((audioElement.currentTime/audioElement.duration)* 100);
+  myProgressbar.value = progress;
+});
+myProgressbar.addEventListener('change', ()=>{
+  audioElement.currentTime = myProgressbar.value * audioElement.duration/100;
 })
